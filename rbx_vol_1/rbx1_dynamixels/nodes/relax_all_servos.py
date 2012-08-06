2db011feac8f726f38f1264dae3f113f47f87650
PiRobotProductions@gmail.com <PiRobotProductions@gmail.com@62658086-da24-ac13-c73a-0a79f790e1ee>
2012-08-06 18:06:27 +0000

    Added head_tracker.launch to rbx1_apps

--- rbx_vol_1/rbx1_dynamixels/nodes/relax_all_servos.py
+++ rbx_vol_1/rbx1_dynamixels/nodes/relax_all_servos.py
@@ -1,8 +1,25 @@
 #!/usr/bin/env python
 
 """
-    Relax all servos by disabling the torque for each and set initial speed
-    to a reasonable value.
+    relax_all_servos.py - Version 0.1 2012-03-24
+    
+    Relax all servos by disabling the torque and setting the speed
+    and torque limit to a moderate values.
+    
+    Created for the Pi Robot Project: http://www.pirobot.org
+    Copyright (c) 2012 Patrick Goebel.  All rights reserved.
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.5
+    
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details at:
+    
+    http://www.gnu.org/licenses/gpl.html
 """
 import roslib
 roslib.load_manifest('rbx1_dynamixels')
